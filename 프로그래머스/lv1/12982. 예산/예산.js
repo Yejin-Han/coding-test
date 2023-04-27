fb43630fa2b477efc5b15071f28186514d29c9c9
Yejin Han (한예진) <119913434+Yejin-Han@users.noreply.github.com>
2023-04-27 23:46:45 +0900

    [level 1] Title: 예산, Time: 0.22 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.js
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -4,12 +4,11 @@ function solution(d, budget) {
     d.sort((a, b) => a - b);
     for(let i = 0; i < d.length; i++) {
         if(budget - d[i] < 0) {
-            result = i;
             break;
         } else {
             budget -= d[i];
         }
-        result = i + 1;
+        result++;
     }
     
     return result;
