82349b1942e32cdf5140ec10c2fe0b195dcc2276
Yejin-Han <yain3333@gmail.com>
2023-04-28 00:03:54 +0900

    230427 다른풀이 update

---
+++ 프로그래머스/lv1/12982. 예산/풀이1.js
@@ -0,0 +1,9 @@
+function solution(d, budget) {
+  return d
+    .sort((a, b) => a - b)
+    .reduce((count, price) => {
+      return count + ((budget -= price) >= 0);
+    }, 0);
+}
+
+/* for문 대신 reduce로 count를 누적 */
